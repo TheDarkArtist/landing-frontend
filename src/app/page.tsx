8597f1a24fc6c83b304948b745fd91ccad9d3eec
Kushagra <sparrow.kushagra@gmail.com>
2025-07-01 12:03:03 +0530

    ui: implement proper landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import styles from "./page.module.css";
+'use client';
+
+import Link from 'next/link';
+import styles from './page.module.css';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    requestAnimationFrame(() => setAnimate(true));
+  }, []);
+
   return (
-    <div className={styles.page}>
-      contentful app
+    <div className={`${styles.page} ${animate ? styles.enter : ''}`}>
+      <h1 className={styles.title}>Contentful App</h1>
+      <nav className={styles.nav}>
+        <Link href="/landing/page-1" className={styles.link}>
+          Go to Page 1 →
+        </Link>
+        <Link href="/landing/page-2" className={styles.link}>
+          Go to Page 2 →
+        </Link>
+      </nav>
     </div>
   );
 }
+
